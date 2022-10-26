5b53b4588bfd6f32eb1d32925f9cfaf8fed89583
HigorSilvaM <107903541+HigorSilvaMiquilino@users.noreply.github.com>
2022-10-26 23:14:40 +0000

    Aprendendo o Input

---
+++ Aula1-2022-10-19.py
@@ -0,0 +1,9 @@
+#Meu primeiro arquivo Python!!!
+print("Hello World")
+
+name = "Higor da Silva Miquilino"
+age =  21
+
+print("My name is " + name + " I'm " + str(age) + " years old")
+
+print(f"I'm {age} years old")
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-#Meu primeiro arquivo Python!!!
-print("Hello World")
+nome = input("Digite seu nome: ")
+print(f"Boa noite, seu nome é {nome}")
 
-name = "Higor da Silva Miquilino"
-age =  21
+print("Quantos anos você tem?")
+idade = int(input("Digite a sua idade: "))
+print(f"Olá {nome} você tem {idade} anos")
 
-print("My name is " + name + " I'm " + str(age) + " years old")
-
-print(f"I'm {age} years old")
+dobro = idade * 2
+print(f"O dobro da idade informada é {dobro}")
